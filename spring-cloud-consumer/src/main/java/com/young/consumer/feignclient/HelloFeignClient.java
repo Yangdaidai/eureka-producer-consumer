0b887b24ce5a67d21af106752df817aeab7ae717
Yangdaidai <1024826067@qq.com>
2020-03-01 15:06:21 +0800

    加入熔断器Hystrix

--- spring-cloud-consumer/src/main/java/com/young/consumer/feignclient/HelloFeignClient.java
+++ spring-cloud-consumer/src/main/java/com/young/consumer/feignclient/HelloFeignClient.java
@@ -1,5 +1,6 @@
 package com.young.consumer.feignclient;
 
+import com.young.consumer.hystrix.HelloFeignHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @Date 2020/2/29 23:51
  * @Version 1.0.0
  **/
-@FeignClient(name = "spring-cloud-producer")
+@FeignClient(name = "spring-cloud-producer", fallback = HelloFeignHystrix.class)
 public interface HelloFeignClient {
     @RequestMapping(value = "/hello")
     String hello(@RequestParam(value = "name") String name);
